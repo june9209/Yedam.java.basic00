611452b05d0ca0bd2625d5924c9c460ac7289f1a
admin <admin@YD5-21>
2021-12-28 09:07:55 +0900

    Hello java

---
+++ hellojava/src/com/yedam/java/main/Hellojava.java
@@ -0,0 +1,12 @@
+package com.yedam.java.main;
+
+import java.util.Iterator;
+
+public class Hellojava {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java!!");	
+		
+	}
+
+}
